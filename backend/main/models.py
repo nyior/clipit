006620143e9584103168e3b050b2e4cbb4e833f4
Nyior Clement Jr <cnyior27@gmail.com>
2021-02-05 01:02:57 +0100

    create client model and modify some tests

--- backend/main/models.py
+++ backend/main/models.py
@@ -4,11 +4,20 @@ from django.db import models
 
 class Client(models.Model):
     """this models the client object(browser) that 
-    shortens a url"""
-    pass
+    sends a long a url"""
+    client_id = models.CharField(max_length=30, unique=True)
+
+    def __unicode__(self):
+        return self.client_id
 
 
 class Url(models.Model):
+    created_by = models.ForeignKey(
+                                    Client, 
+                                    on_delete=models.SET_NULL,
+                                    related_name="urls",
+                                    null=True
+                                    )
     long_url = models.URLField(max_length=500)
     shortcode = models.CharField(max_length=30, unique=True)
     hits = models.IntegerField(default=0)
