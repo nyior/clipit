44abf0023c9475bbe2dbeb9bc1ef78211b87f3cc
Nyior Clement Jr <cnyior27@gmail.com>
2021-02-07 09:20:04 +0100

    replace axios with fetch for api requests

--- frontend/src/utils/api.service.js
+++ frontend/src/utils/api.service.js
@@ -4,20 +4,38 @@ const API_URL = "https://shter.herokuapp.com";
 
 // axios.defaults.withCredentials = true
 
-function apiService(endpoint, method, data) {
-  endpoint = `${API_URL}/${endpoint}`;
+async function apiService(endpoint, method, data) {
+    endpoint = `${API_URL}/${endpoint}`;
 
-  const config = {
-    url: endpoint,
-    method: method,
-    withCredentials: true,
-    data: data !== undefined ? data : null,
-    headers: {
-      "content-type": "application/json"
-    }
-  };
+    const response = await fetch(
+        endpoint, 
+        {
+            method: method,
+            credentials:"include",
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: data !== undefined ? data : null,
+        }
+    );
 
-  return axios(config).then(response => response.data);
+    return response.data; 
 }
+
+// function apiService(endpoint, method, data) {
+//   endpoint = `${API_URL}/${endpoint}`;
+
+//   const config = {
+//     url: endpoint,
+//     method: method,
+//     withCredentials: true,
+//     data: data !== undefined ? data : null,
+//     headers: {
+//       "content-type": "application/json"
+//     }
+//   };
+
+//   return axios(config).then(response => response.data);
+// }
 
 export { apiService };
